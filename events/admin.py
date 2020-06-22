a1e21e6ae9be29df6517f196c9e83c2be9a1691d
sajib1066 <sajib1066@gmail.com>
2020-06-22 18:06:19 +0600

    create event model

--- events/admin.py
+++ events/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import EventCategory
+from .models import EventCategory, Event
 
 admin.site.register(EventCategory)
+admin.site.register(Event)
--- events/models.py
+++ events/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from mapbox_location_field.models import LocationField
 
 
 class EventCategory(models.Model):
@@ -18,4 +19,29 @@ class EventCategory(models.Model):
     
     def get_absolute_url(self):
         return reverse('event-category-list')
+
+
+class Event(models.Model):
+    category = models.ForeignKey(EventCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, unique=True)
+    uid = models.PositiveIntegerField(unique=True)
+    description = models.TextField()
+    scheduled_status = models.IntegerField()
+    venue = models.CharField(max_length=255)
+    agenda = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    location = LocationField()
+    points = models.PositiveIntegerField()
+    maximum_attende = models.PositiveIntegerField()
+    created_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='event_created_user')
+    updated_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='event_updated_user')
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now_add=True)
+    status = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
 
