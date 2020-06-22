c2c560ed48300d7a3357bc09245a5076afea1642
sajib1066 <sajib1066@gmail.com>
2020-06-22 15:40:27 +0600

    create event category model

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import EventCategory
+
+admin.site.register(EventCategory)
--- events/models.py
+++ events/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class EventCategory(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    code = models.CharField(max_length=6, unique=True)
+    image = models.ImageField(upload_to='event_category/')
+    priority = models.IntegerField(unique=True)
+    created_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='created_user')
+    updated_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='updated_user')
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now_add=True)
+    status = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
