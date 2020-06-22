f9260c5539ffaf7df2d313232948cf104956e90a
sajib1066 <sajib1066@gmail.com>
2020-06-22 15:25:53 +0600

    create events app

--- roche/settings.py
+++ roche/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local Apps
+    'events.apps.EventsConfig',
 ]
 
 MIDDLEWARE = [
