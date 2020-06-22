db06e38a91d3d0bec1db643f9d70e970700e8066
sajib1066 <sajib1066@gmail.com>
2020-06-22 23:03:54 +0600

    create all event models

--- events/admin.py
+++ events/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from mapbox_location_field.admin import MapAdmin
 
-from .models import EventCategory, Event
+from .models import (
+    EventCategory,
+    Event,
+    JobCategory,
+    EventJobCategoryLinking,
+    EventMember,
+    EventUserWishList,
+    EventImage,
+    UserCoin,
+    EventNotofication,
+    EventNotoficationHistory,
+)
 
 admin.site.register(EventCategory)
 admin.site.register(Event, MapAdmin)
+admin.site.register(JobCategory)
+admin.site.register(EventJobCategoryLinking)
+admin.site.register(EventMember)
+admin.site.register(EventUserWishList)
+admin.site.register(EventImage)
+admin.site.register(UserCoin)
+admin.site.register(EventNotofication)
+admin.site.register(EventNotoficationHistory)
--- events/models.py
+++ events/models.py
@@ -47,4 +47,104 @@ class Event(models.Model):
         return reverse('event-list')
 
 
+class JobCategory(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class EventJobCategoryLinking(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    job_category = models.ForeignKey(JobCategory, on_delete=models.CASCADE)
+    status = models.IntegerField()
+
+    def __str__(self):
+        return str(self.event)
+
+
+class EventMember(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    attend_status = models.IntegerField()
+    created_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='eventmember_created_user')
+    updated_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='eventmember_updated_user')
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now_add=True)
+    status = models.IntegerField()
+
+    def __str__(self):
+        return str(self.event)
+
+
+class EventUserWishList(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    created_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='eventwishlist_created_user')
+    updated_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='eventwishlist_updated_user')
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now_add=True)
+    status = models.IntegerField()
+
+    def __str__(self):
+        return str(self.event)
+
+
+class EventImage(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='event_image/')
+    position = models.PositiveIntegerField(unique=True)
+    created_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='eventimage_created_user')
+    updated_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='eventimage_updated_user')
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now_add=True)
+    status = models.IntegerField()
+
+    def __str__(self):
+        return str(self.event)
+
+
+class UserCoin(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    gain_type = models.PositiveIntegerField()
+    gain_coin = models.PositiveIntegerField()
+    created_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='usercoin_created_user')
+    updated_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='usercoin_updated_user')
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now_add=True)
+    status = models.IntegerField()
+
+    def __str__(self):
+        return str(self.user)
+
+
+class EventNotofication(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    created_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='eventnotification_created_user')
+    updated_user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='eventnotification_updated_user')
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now_add=True)
+    status = models.IntegerField()
+
+    def __str__(self):
+        return str(self.event)
+
+
+class EventNotoficationHistory(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    notification = models.ForeignKey(EventNotofication, on_delete=models.CASCADE)
+    user_view_status = models.PositiveIntegerField()
+    user_view_date = models.DateField(auto_now_add=True)
+    status = models.IntegerField()
+
+    def __str__(self):
+        return str(self.notification)
+
+
+
+
+
+
+
+
 
